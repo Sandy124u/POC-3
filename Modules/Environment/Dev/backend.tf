f4826c78c1f18c8aca96ad861799c5f94ce2d9c4
Sandy124u <sandy.net89@gmail.com>
2026-06-06 23:46:47 +0530

    S3 region extra comma

--- Modules/Environment/Dev/backend.tf
+++ Modules/Environment/Dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "corp-terraform-state"
     key            = "ec2/dev/terraform.tfstate"
-    region         = "us-west-2 "
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
